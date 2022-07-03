6750eb903e12c8c79e25c0e72d5b0d3a95e977d6
juraj <jurajj@txodds.com>
2022-07-03 14:19:02 +0200

    fix url maybe

--- test.js
+++ test.js
@@ -15,28 +15,12 @@ let headers = {
 }
 
 
-let url = new URL("https://node.bolt.eu/rental-search/categoriesOverview")
-url.searchParams.set("country", "sk")
-url.searchParams.set("gps_lat", "49.20053758427728")
-url.searchParams.set("gps_lng", "16.595202877658718")
-url.searchParams.set("device_os_version", "iOS15.5")
-url.searchParams.set("deviceId", "60E3650B-071D-4DDA-AB99-EA22285B8F63")
-url.searchParams.set("select_all", "true")
-url.searchParams.set("deviceType", "iphone")
-url.searchParams.set("lat", "49.20053769305785")
-url.searchParams.set("language", "en-GB")
-url.searchParams.set("lng", "16.595203019678593")
-url.searchParams.set("payment_method_id", "PML6JXBPJ9C29X42")
-url.searchParams.set("device_name", "iPhone14,4")
-url.searchParams.set("session_id", "34730400u1656838818")
-url.searchParams.set("version", "CI.59.0")
-url.searchParams.set("user_id", "34730400")
-url.searchParams.set("payment_method_type", "adyen")
+let url = `https://node.bolt.eu/rental-search/categoriesOverview?country=sk&gps_lat=49.20053758427728&gps_lng=16.595202877658718&device_os_version=iOS15.5&deviceId=60E3650B-071D-4DDA-AB99-EA22285B8F63&select_all=true&deviceType=iphone&lat=49.20053769305785&language=en-GB&lng=16.595203019678593&payment_method_id=PML6JXBPJ9C29X42&device_name=iPhone14,4&session_id=34730400u1656838818&version=CI.59.0&user_id=34730400&payment_method_type=adyen`
 
-cosnole.log(url.toString())
+cosnole.log(url)
 
 
-let req = new Request(url.toString())
+let req = new Request(url)
 req.headers = headers
 
 cosnole.log(req)
