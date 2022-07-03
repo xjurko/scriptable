de6054b14c0daed32a5ed394aacc159978426c68
juraj <jurajj@txodds.com>
2022-07-03 14:32:06 +0200

    extract vehicles

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
 let creds = importModule("creds")
 
-let x = 5
-
-
 let headers = {
 	"host": "node.bolt.eu",
 	"cache-control": "no-cache",
@@ -17,13 +14,16 @@ let headers = {
 
 let url = `https://node.bolt.eu/rental-search/categoriesOverview?country=sk&gps_lat=49.20053758427728&gps_lng=16.595202877658718&device_os_version=iOS15.5&deviceId=60E3650B-071D-4DDA-AB99-EA22285B8F63&select_all=true&deviceType=iphone&lat=49.20053769305785&language=en-GB&lng=16.595203019678593&payment_method_id=PML6JXBPJ9C29X42&device_name=iPhone14,4&session_id=34730400u1656838818&version=CI.59.0&user_id=34730400&payment_method_type=adyen`
 
-cosnole.log(url)
+console.log(url)
 
 
 let req = new Request(url)
 req.headers = headers
 
-cosnole.log(req)
+console.log(req)
 
-let resp = await req.loadJJSON()
-console.log(resp)
+let resp = await req.loadJSON()
+
+vehicles = resp["data"]["categories"].flatMap(cat => cat["vehicles"])
+
+console.log(vehicles)
