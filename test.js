00e61bd6fc1094ae72e4b2d5a89dd3202e1e8b56
juraj <jurajj@txodds.com>
2022-07-03 11:53:52 +0200

    fix url maybe

--- test.js
+++ test.js
@@ -14,24 +14,30 @@ let headers = {
 	"accept-encoding": "gzip, deflate, br"
 }
 
-let req = new Request("https://node.bolt.eu/rental-search/categoriesOverview");
+
+let url = new URL("https://node.bolt.eu/rental-search/categoriesOverview")
+url.searchParams.set("country", "sk")
+url.searchParams.set("gps_lat", "49.20053758427728")
+url.searchParams.set("gps_lng", "16.595202877658718")
+url.searchParams.set("device_os_version", "iOS15.5")
+url.searchParams.set("deviceId", "60E3650B-071D-4DDA-AB99-EA22285B8F63")
+url.searchParams.set("select_all", "true")
+url.searchParams.set("deviceType", "iphone")
+url.searchParams.set("lat", "49.20053769305785")
+url.searchParams.set("language", "en-GB")
+url.searchParams.set("lng", "16.595203019678593")
+url.searchParams.set("payment_method_id", "PML6JXBPJ9C29X42")
+url.searchParams.set("device_name", "iPhone14,4")
+url.searchParams.set("session_id", "34730400u1656838818")
+url.searchParams.set("version", "CI.59.0")
+url.searchParams.set("user_id", "34730400")
+url.searchParams.set("payment_method_type", "adyen")
+
+cosnole.log(url.toString())
+
+
+let req = new Request(url.toString())
 req.headers = headers
-req.addParameterToMultipart("country", "sk")
-req.addParameterToMultipart("gps_lat", "49.20053758427728")
-req.addParameterToMultipart("gps_lng", "16.595202877658718")
-req.addParameterToMultipart("device_os_version", "iOS15.5")
-req.addParameterToMultipart("deviceId", "60E3650B-071D-4DDA-AB99-EA22285B8F63")
-req.addParameterToMultipart("select_all", "true")
-req.addParameterToMultipart("deviceType", "iphone")
-req.addParameterToMultipart("lat", "49.20053769305785")
-req.addParameterToMultipart("language", "en-GB")
-req.addParameterToMultipart("lng", "16.595203019678593")
-req.addParameterToMultipart("payment_method_id", "PML6JXBPJ9C29X42")
-req.addParameterToMultipart("device_name", "iPhone14,4")
-req.addParameterToMultipart("session_id", "34730400u1656838818")
-req.addParameterToMultipart("version", "CI.59.0")
-req.addParameterToMultipart("user_id", "34730400")
-req.addParameterToMultipart("payment_method_type", "adyen")
 
 cosnole.log(req)
 
